526298f368ce4866fbe70708d8ddd4a413e417f0
Raju-ranjan-nitp <raju.ranjan14@gmail.com>
2023-06-09 13:20:11 +0530

    polymorphism 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-public class gitMain {
+public class Main {
     public static void main(String[] args) {
         Product p = new Product();
         int result1 = p.product(2,3);
