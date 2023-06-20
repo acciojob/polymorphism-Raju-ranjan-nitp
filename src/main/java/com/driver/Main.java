6cd1037ab56c8c4af8e027ad8ee9020438f9d516
Raju-ranjan-nitp <raju.ranjan14@gmail.com>
2023-06-20 12:39:23 +0530

    polymorphism2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,29 +1,33 @@
 package com.driver;
-
 public class Main {
-    public static void main(String[] args) {
-        Product p = new Product();
-        int result1 = p.product(2,3);
-        System.out.println(result1);
 
-        int result2 = p.product(2,3,4);
-        System.out.println(result2);
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
-        double result3 = p.product(2.5,3.5);
-        System.out.println(result3);
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
     }
 
-  static class Product{
-      public int product(int x, int y){
-          return x*y;
-      }
+    public static void main(String[] args) {
+        Product p = new Product();
 
-      public int product(int x, int y, int z){
-          return x*y*z;
-      }
+        // call the first method
+        int result1 = p.product(3, 4);
+        System.out.println("Result 1: " + result1);
 
-      public double product(double x, double y){
-          return x*y;
-      }
-  }
+        // call the second method
+        int result2 = p.product(2, 5, 7);
+        System.out.println("Result 2: " + result2);
+
+        // call the third method
+        double result3 = p.product(1.5, 2.5);
+        System.out.println("Result 3: " + result3);
+    }
 }
